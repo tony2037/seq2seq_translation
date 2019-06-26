3a25bbcca4b069bfd6b82a86f5607f5eaecd6b94
ztex <ztex030640417@gmail.com>
2019-06-26 10:09:16 +0800

    Feat Import something

--- seq2seq_bot.py
+++ seq2seq_bot.py
@@ -13,3 +13,17 @@
     Attention mechanism <https://arxiv.org/abs/1409.0473>`__, which lets the decoder learn to focus over a specific range of the input sequence.
 
 """
+
+from __future__ import unicode_literals, print_function, division
+from io import open
+import unicodedata
+import string
+import re
+import random
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
