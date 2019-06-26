18446c4f653ed0d3ac9df63889ab39877f3adeb1
ztex <ztex030640417@gmail.com>
2019-06-26 16:10:38 +0800

    Feat Data preprocessing and read data

--- seq2seq_bot.py
+++ seq2seq_bot.py
@@ -27,3 +27,132 @@ from torch import optim
 import torch.nn.functional as F
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+######################################################################
+# Loading data files
+# ==================
+
+SOS_token = 0
+EOS_token = 1
+
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2  # Count SOS and EOS
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+######################################################################
+# The files are all in Unicode, to simplify we will turn Unicode
+# characters to ASCII, make everything lowercase, and trim most
+# punctuation.
+#
+
+# Turn a Unicode string to plain ASCII, thanks to
+# https://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+# Lowercase, trim, and remove non-letter characters
+
+
+def normalizeString(s):
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    return s
+
+
+######################################################################
+# To read the data file we will split the file into lines, and then split
+# lines into pairs. The files are all English → Other Language, so if we
+# want to translate from Other Language → English I added the ``reverse``
+# flag to reverse the pairs.
+#
+
+def readLangs(lang1, lang2, reverse=False):
+    print("Reading lines...")
+
+    # Read the file and split into lines
+    lines = open('data/%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
+        read().strip().split('\n')
+
+    # Split every line into pairs and normalize
+    pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
+
+    # Reverse pairs, make Lang instances
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(lang2)
+        output_lang = Lang(lang1)
+    else:
+        input_lang = Lang(lang1)
+        output_lang = Lang(lang2)
+
+    return input_lang, output_lang, pairs
+
+######################################################################
+#We get rid of the samples that the length are bigger than MAX_LENGTH
+#
+
+MAX_LENGTH = 128
+
+def filterPair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and \
+        len(p[1].split(' ')) < MAX_LENGTH
+
+
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+
+######################################################################
+# The full process for preparing the data is:
+#
+# -  Read text file and split into lines, split lines into pairs
+# -  Normalize text, filter by length and content
+# -  Make word lists from sentences in pairs
+#
+
+def prepareData(lang1, lang2, reverse=False):
+    input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
+    print("Read %s sentence pairs" % len(pairs))
+    pairs = filterPairs(pairs)
+    print("Trimmed to %s sentence pairs" % len(pairs))
+    print("Counting words...")
+    for pair in pairs:
+        input_lang.addSentence(pair[0])
+        output_lang.addSentence(pair[1])
+    print("Counted words:")
+    print(input_lang.name, input_lang.n_words)
+    print(output_lang.name, output_lang.n_words)
+    return input_lang, output_lang, pairs
+
+input_lang, output_lang, pairs = prepareData('A', 'Q', True)
+print(random.choice(pairs))
+
+######################################################################
+# The Encoder
+# -----------
+#
+# The encoder of a seq2seq network is a RNN that outputs some value for
+# every word from the input sentence. For every input word the encoder
+# outputs a vector and a hidden state, and uses the hidden state for the
+# next input word.
+#
