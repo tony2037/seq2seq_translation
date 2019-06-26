168c0ad26a8a86b5890c1a4165b7de5f54dff0aa
ztex <ztex030640417@gmail.com>
2019-06-26 10:04:59 +0800

    Feat Add a new script about bot

---
+++ seq2seq_bot.py
@@ -0,0 +1,15 @@
+"""
+**Reference**: `Sean Robertson <https://github.com/spro/practical-pytorch>`_
+
+::
+
+    [KEY: > input, = target, < output]
+
+    > how are glacier caves formed .
+    = A partly submerged glacier cave on Perito Moreno Glacier .
+    < A partly submerged glacier cave on Perito Moreno Glacier .
+
+... Attention mechanism
+    Attention mechanism <https://arxiv.org/abs/1409.0473>`__, which lets the decoder learn to focus over a specific range of the input sequence.
+
+"""
