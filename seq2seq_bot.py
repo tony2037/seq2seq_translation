af0d741edbecdf111a1a593f3bed076f5d5e8aad
ztex <ztex030640417@gmail.com>
2019-06-27 11:14:39 +0800

    Feat Add Encoder and Attention mechinasm Decoder

--- seq2seq_bot.py
+++ seq2seq_bot.py
@@ -82,9 +82,7 @@ def normalizeString(s):
 
 ######################################################################
 # To read the data file we will split the file into lines, and then split
-# lines into pairs. The files are all English → Other Language, so if we
-# want to translate from Other Language → English I added the ``reverse``
-# flag to reverse the pairs.
+# lines into pairs.
 #
 
 def readLangs(lang1, lang2, reverse=False):
@@ -155,4 +153,94 @@ print(random.choice(pairs))
 # every word from the input sentence. For every input word the encoder
 # outputs a vector and a hidden state, and uses the hidden state for the
 # next input word.
+#
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+######################################################################
+# Attention Decoder
+# ^^^^^^^^^^^^^^^^^
+#
+# If only the context vector is passed betweeen the encoder and decoder,
+# that single vector carries the burden of encoding the entire sentence.
+#
+# Attention allows the decoder network to "focus" on a different part of
+# the encoder's outputs for every step of the decoder's own outputs. First
+# we calculate a set of *attention weights*. These will be multiplied by
+# the encoder output vectors to create a weighted combination. The result
+# (called ``attn_applied`` in the code) should contain information about
+# that specific part of the input sequence, and thus help the decoder
+# choose the right output words.
+#
+# Calculating the attention weights is done with another feed-forward
+# layer ``attn``, using the decoder's input and hidden state as inputs.
+# Because there are sentences of all sizes in the training data, to
+# actually create and train this layer we have to choose a maximum
+# sentence length (input length, for encoder outputs) that it can apply
+# to. Sentences of the maximum length will use all the attention weights,
+# while shorter sentences will only use the first few.
+#
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+        super(AttnDecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input, hidden, encoder_outputs):
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)
+
+        attn_weights = F.softmax(
+            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+######################################################################
+# Training
+# ========
+#
+# Preparing Training Data
+# -----------------------
+#
+# To train, for each pair we will need an input tensor (indexes of the
+# words in the input sentence) and target tensor (indexes of the words in
+# the target sentence). While creating these vectors we will append the
+# EOS token to both sequences.
 #
